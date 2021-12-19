02e0881cdc73895087aa8241708bb9b233acee05
Mikhail <zhushman.m.s@mail.ru>
2021-12-19 15:36:15 +0300

    fix brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-import run from '../src/index.js';
-import { gameRule, game } from '../src/games/even.js';
+import runGame from '../src/games/even.js';
 
-run(gameRule, game);
+runGame();
--- src/games/even.js
+++ src/games/even.js
@@ -1,4 +1,5 @@
 import getRandInt from '../utils/getRandInt.js';
+import { roundСount, engine } from '../engine.js';
 
 const isEven = (int) => {
   if (int % 2 === 0) {
@@ -7,14 +8,19 @@ const isEven = (int) => {
   return 'no';
 };
 
-const gameRule = 'Answer \'yes\' if the number is even, otherwise answer \'no\'.';
+const gameDescription = 'Answer \'yes\' if the number is even, otherwise answer \'no\'.';
 
 const game = () => {
   const randInt = getRandInt(1, 100);
   const quest = randInt;
   const answer = isEven(randInt);
-  const QA = [quest, answer];
-  return QA;
+  return [quest, answer];
 };
 
-export { gameRule, game };
+export default () => {
+  const rounds = [];
+  for (let i = 0; i < roundСount; i += 1) {
+    rounds.push(game());
+  }
+  return engine(gameDescription, rounds);
+};
