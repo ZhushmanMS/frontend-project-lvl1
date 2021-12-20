c7240fdf7a4f2737210770fcb613a12b4e69dd6e
Mikhail <zhushman.m.s@mail.ru>
2021-12-20 18:51:14 +0300

    reforge isPrime

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,12 +1,15 @@
 import { getRandomInt, generateRoundsData } from '../utils.js';
 
 const isPrime = (int) => {
-  const arrPrimeInts = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
-    41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
-  if (arrPrimeInts.includes(int)) {
-    return 'yes';
+  if (int < 2) {
+    return 'no';
   }
-  return 'no';
+  for (let i = 2; i <= int / 2; i += 1) {
+    if (int % i === 0) {
+      return 'no';
+    }
+  }
+  return 'yes';
 };
 
 const gameDescription = 'Answer \'yes\' if given number is prime. Otherwise answer \'no\'.';
