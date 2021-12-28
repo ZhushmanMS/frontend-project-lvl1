42f5acefc574130afff06265b206edd5f207c8a3
zero <zhushman.m.s@mail.ru>
2021-12-28 10:10:08 +0400

    merge getProgression

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,27 +1,23 @@
 import getRandomInt from '../utils.js';
 import { roundСount, engine } from '../engine.js';
 
-const getProgression = (startInt, step, length) => {
-  const progression = [];
-  let temp = startInt;
-  for (let i = 0; i < length; i += 1) {
-    progression.push(temp);
-    temp += step;
-  }
-  return progression;
-};
-
 const gameDescription = 'What number is missing in the progression?';
 
 const generateRound = () => {
   const startInt = getRandomInt(1, 9);
   const step = getRandomInt(2, 9);
   const length = getRandomInt(5, 15);
+  const progression = [];
+  let temp = startInt;
+
+  for (let i = 0; i < length; i += 1) {
+    progression.push(temp);
+    temp += step;
+  }
   const answerPosition = getRandomInt(0, length - 1);
-  const gameProgression = getProgression(startInt, step, length);
-  const answer = String(gameProgression[answerPosition]);
-  gameProgression[answerPosition] = '..';
-  const question = gameProgression.join(' ');
+  const answer = String(progression[answerPosition]);
+  progression[answerPosition] = '..';
+  const question = progression.join(' ');
   return [question, answer];
 };
 
